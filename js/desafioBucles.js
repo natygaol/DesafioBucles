4e6220307218c1f95cc1313367566426a8c92719
nataly garcia <naty_garcioli@hotmail.com>
2022-01-12 17:11:52 -0500

    primera parte lista

---
+++ js/desafioBucles.js
@@ -0,0 +1,33 @@
+// Imprimir impares 1-20
+
+for(let i=1; i <= 20; i++){
+  console.log(i);
+} 
+
+// Disminuir múltiplos de 3, del 0 al 100
+
+for(let i=0; i <= 100; i++){
+  if( i % 3 == 0){
+    console.log(i);
+  }
+}
+
+// Imprime la secuencia: 4, 2.5, 1, -0.5, -2, -3.5
+
+for(let i = 4; i <= 4; i -= 1.5){
+  if( i <= 4 && i >= -3.5){
+    console.log(i);
+  }
+}
+
+// Sigma -> sumar valores del 1 al 100
+
+let sum = 0;
+
+for(let i = 0; i <= 100; i++){
+  sum += i
+  console.log(i);
+}
+
+
+// Factorial -> Multiplicar del 1 al 12 en una variable product
